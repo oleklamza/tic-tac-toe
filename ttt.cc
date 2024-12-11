0a3fe64f7e22544670628f6250e84251b8e3f29f
olekl <olek.lamza@gmail.com>
2024-12-11 20:55:57 +0100

    Wprowadzanie współrzędnych przez gracza, wypełnianie planszy

--- ttt.cc
+++ ttt.cc
@@ -3,23 +3,21 @@ using namespace std;
 
 #define BOARD_SIZE 3
 
+// enumeracja przechowująca wartości pól planszy;
+// dzięki niej w kodzie posługujemy się symbolami,
+// a nie tajemniczymi liczbami (magic numbers!)
 enum Square {
     EMPTY,
     O,
     X
 };
 
+// plansza
+int board[BOARD_SIZE][BOARD_SIZE] = {Square::EMPTY};
 
-int main()
-{
-    // plansza
-    int board[BOARD_SIZE][BOARD_SIZE] = {Square::EMPTY};
-
-    // przykładowe wypełnienie planszy
-    board[0][1] = Square::O;
-    board[2][1] = Square::X;
-
-    // rysowanie planszy
+// rysowanie planszy przeniesione do funkcji
+void draw()
+{   
     cout << "  a b c\n";
     for (int row=0; row<BOARD_SIZE; row++) {
         cout << row+1 << " ";
@@ -37,6 +35,44 @@ int main()
             cout << c << " ";
         }
         cout << "\n";
+    }
+}
+
+int main()
+{
+    // inicjalizacja generatora liczb pseudolosowych
+    // potrzebnego do losowania pola
+    srand(time(nullptr));
+    // wywołanie funkcji rysującej planszę
+    draw();
+
+    // pętla rund
+    while (true) {
+        // ruch gracza
+        // użytkownik podaje współrzędne jako łańcuch <kolumna><wiersz>, np. a2
+        
+        // pętla wprowadzania współrzędnych
+        int col, row;
+        while (true) {
+            cout << "\nPodaj współrzędne: ";
+            string s;
+            cin >> s;
+            // sprawdzenie poprawności wprowadzonych danych
+            // jeżeli są niepoprawne, pętla while wykonuje ponowną iterację
+            // jeżeli są poprawne, wyskakujemy z pętli (break)
+            if (s.length() == 2) {
+                col = s[0] - 'a';
+                row = s[1] - '1';
+
+                if ( (col >= 0 && col <= 2) && (row >= 0 && row <= 2) )
+                    break;
+            }
+            cout << "Niepoprawne współrzędne. Ma być [a-c][1-3]." << endl;
+        }
+        
+        // ustawienie pola i narysowanie planszy
+        board[row][col] = Square::O;
+        draw();
     }
 
     return 0;
